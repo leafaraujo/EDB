9d77e3b5010b218d6c4288dddc8a67b94bdd28cf
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-04-18 18:30:52 -0300

    Create fibonnaci.cpp

---
+++ exercises/fibonnaci.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+  int number;
+  cout << "---------------------------------------- Fibonnaci Simulator ----------------------------------------" << endl;
+  cout << "Olá, digite um número N e iremos encontrar qual valor está nessa posição na sequência de fibbonaci :p" << endl;
+  cin >> number;
+  createFibonnaci(number)
+  return 0;
+}

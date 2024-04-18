c2b3d813e0ec7c19e462df80e4fca61c22633132
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-04-18 18:59:12 -0300

    Update fibonnaci.cpp

--- exercises/fibonnaci.cpp
+++ exercises/fibonnaci.cpp
@@ -1,12 +1,13 @@
 # include <iostream>
 using namespace std;
 
-
 int main(){
-  int number;
+  int number, position;
   cout << "---------------------------------------- Fibonnaci Simulator ----------------------------------------" << endl;
-  cout << "Olá, digite um número N e iremos encontrar qual valor está nessa posição na sequência de fibbonaci :p" << endl;
-  cin >> number;
-  createFibonnaci(number)
+  cout << "Olá, digite um número N e iremos encontrar qual valor está nessa posição na sequência de fibbonaci :p";
+  cin >> position;
+  cout << endl;
+  
+  number = searchFibonnaci(position)
   return 0;
 }
